00bfec142e72bb91ed6bca7d7c5bf75fd3c7752a
Mateus <mateus.bs.rangel@gmail.com>
2020-01-30 16:53:56 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <h1>ScoreTasks</h1>
+    
+      <div className='content'>
+        <form>
+          <label htmlFor="task"></label>
+          <input
+            id="task"
+            type="task"
+            placeholder="Crie uma nova tarefa"
+          />
+
+          <button className="btn" type="submit">Criar</button>
+        </form>
+      </div>
     </div>
   );
+
+  
 }
 
 export default App;
---
+++ src/components/TaskForm/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+function TaskForm({ onSubmit }) {
+    const [taskName, setTaskName] = useState('');
+
+    
+
+}
+
+export default TaskForm;
